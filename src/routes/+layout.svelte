5e22326d9026c075f4dee232e966d4ed63eaa16e
PalaBeaveR <PalaBeaveR@gmail.com>
2023-01-28 19:58:21 +0200

    Added basic paralax

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,31 @@
-<script>
-	import Header from './Header.svelte';
-	import './styles.css';
+<script lang="ts">
+	import Header from './Header.svelte'
+	import './styles.css'
+
+    // let variable_name: variable_type = variable_value
+    let paralax_offset: String = "0px"
+
+    // можеш поменять переменную чтоби изменить скорость паралакса
+    let paralax_ratio: number = 1
+
+    function paralax() {
+        // здесь ми находим насколько прокручена страница
+        // || означаєт ИЛИ тоесть если window.scrollY не существуєт ми берем window.scrollTop
+        // || здесь используєтся потому что в разних браузерах в разних местах хранится прокрутка такчто || позволяєт коду работать во всех браузерах
+        // про (<any>window) пока не волнуйся ето я убегаю от проги которая говорит где у меня ошибки в коде
+        let scrollPos: number = window.scrollY || (<any>window).scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+
+        // здесь px прибавляєтся в конце потому что calc  не работаєт с еденицами без розмерок
+        paralax_offset = scrollPos * paralax_ratio + "px"
+    }
 </script>
 
-
+<!-- здесь ми заставляєм код ждать пока страница не будет крутится і когда она крутится ми виполняєм функцию paralax-->
+<svelte:window on:scroll={paralax}/>
 
 <div class="app">
-	<div class="gradient">
-	
-	</div>
+    <!-- все что в {} ето JS код. Такчто можно взять переменную і вставить в HTML -->
+	<div style="--offset: {paralax_offset}" class="gradient"/>
 	<Header />
 
 	<main>
@@ -21,17 +38,19 @@
 </div>
 
 <style>
-	.gradient{
+	.gradient {
 		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100vw;
 		height: 100%;
-		background: linear-gradient(rgba(1,1,1,0), 10%,  purple);
+        /* здесь ми можем взять переменную которую засунули в HTML і с помощью calc посчитать паралакс */
+		background: linear-gradient(rgba(1, 1, 1, 0) calc(55vh - var(--offset)), purple calc(65vh - var(--offset)));
 		z-index: -1;
-
 	}
+
 	.app {
+        position: relative;
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
