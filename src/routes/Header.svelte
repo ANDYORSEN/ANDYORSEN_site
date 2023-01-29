c702a0610e39a61de3a1906762361a67145b70c6
PalaBeaveR <PalaBeaveR@gmail.com>
2023-01-29 21:09:50 +0200

    Tweaked contents going out of bounds and cleaned up the header styles

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,4 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header>
@@ -21,14 +18,15 @@
 </header>
 
 <style>
-	.logo{
+	.logo {
 		font-size: 1.5em;
 	}
 	.left {
 		justify-content: end;
 	}
-	div{
+	div {
 		flex: 1;
+        overflow: hidden;
 		display: flex;
 		width: auto;
 	}
@@ -42,72 +40,13 @@
 		justify-content: space-between;
 	}
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
 	nav {
-		height: 4.5vw;
+		height: 5vh;
 		width: 100vw;
 		background-color: mediumpurple;
 		display: flex;
 		justify-content: center;
 		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
 	}
 
 	nav a {
